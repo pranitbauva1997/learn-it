0f2614717fc0f1c588dd200d73421c71dc0af1b0
Pranit Bauva <pranit.bauva@gmail.com>
2017-04-14 15:41:14 +0530

    main: add help command to main
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include "common.h"
 
-int main() {
-    help();
+int main(int argc, const char **argv) {
+    int i;
+
+    if (argc < 2)
+        return print_help();
+
+    for (i = 0; i < argc; i++) {
+        if (!strcmp(argv[i], "help") || !strcmp(argv[i], "-h") ||
+            !strcmp(argv[i], "--help")) {
+            return print_help();
+        } else {
+            fprintf(stderr, "error: sorry couldn't identify what you are\n"
+                            "       saying. Please try `learn-it help`\n");
+            return -1;
+        }
+    }
 }
